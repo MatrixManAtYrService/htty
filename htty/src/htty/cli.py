0bf19147b2c53d146d93cdadd01cce67d8d86532
Matt Rixman <MatrixManAtYrService@users.noreply.github.com>
2025-07-14 16:00:01 -0600

    Version: 0.2.18

--- htty/src/htty/cli.py
+++ htty/src/htty/cli.py
@@ -106,7 +106,7 @@ processed in order.
         # import os
         # cog.out(f'version="{os.environ["HTTY_VERSION_INFO_HTTY"]}",')
         # ]]]
-        version="htty 0.2.17 (unknown)",
+        version="htty 0.2.18 (unknown)",
         # [[[end]]]
     )
     parser.add_argument(
--- nix/lib/version.nix
+++ nix/lib/version.nix
@@ -17,7 +17,7 @@ let
   # ]]]
   major = 0;
   minor = 2;
-  patch = 17;
+  patch = 18;
   prerelease = ""; # Set by --prerelease, empty for stable releases
   # [[[end]]]
 
