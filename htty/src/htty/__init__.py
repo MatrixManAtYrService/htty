fb690008992a95079fdd44a628a8e56a4521d0b3
Matt Rixman <MatrixManAtYrService@users.noreply.github.com>
2025-07-14 15:43:02 -0600

    Version: 0.2.17

--- htty/src/htty/__init__.py
+++ htty/src/htty/__init__.py
@@ -148,7 +148,7 @@ from htty.keys import Press
 # import os
 # cog.out(f'__version__ = "{os.environ["HTTY_VERSION"]}"')
 # ]]]
-__version__ = "0.2.16"
+__version__ = "0.2.17"
 # [[[end]]]
 
 __all__ = [
