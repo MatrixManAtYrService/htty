50a3c50032116c3f676be9bcd595e35be33cb1a5
Matt Rixman <MatrixManAtYrService@users.noreply.github.com>
2025-07-14 21:42:18 -0600

    Version: 0.2.25

--- htty/src/htty/cli.py
+++ htty/src/htty/cli.py
@@ -106,7 +106,7 @@ processed in order.
         # import os
         # cog.out(f'version="{os.environ["HTTY_VERSION_INFO_HTTY"]}",')
         # ]]]
-        version="htty 0.2.23 (unknown)",
+        version="htty 0.2.25 (unknown)",
         # [[[end]]]
     )
     parser.add_argument(
